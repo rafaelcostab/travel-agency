4b09c9fa9070b89c33f427611bcade104721f536
Rafael Costa <rafael.costab@hotmail.com>
2023-02-07 23:51:26 -0300

    criando serviço de cliente

--- src/main/java/com/rafaelcostab/client/Client.java
+++ src/main/java/com/rafaelcostab/client/Client.java
@@ -1,5 +1,14 @@
 package com.rafaelcostab.client;
 
-public class Client {
+import javax.persistence.Entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+@Entity
+public class Client extends PanacheEntity{
     
+    //nao utilizaremos id, pois o panache gerencia o id
+    
+    public String name;
+
 }
